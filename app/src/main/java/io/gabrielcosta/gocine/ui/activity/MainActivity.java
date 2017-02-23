f3d367bc223e540a7198e08eeaef99943af41ba5
Gabriel Costa <gabrielcosta08@gmail.com>
2017-02-22 23:56:55 -0300

    Create ui/activity package
    Move Mainactivity to it

--- app/src/main/java/io/gabrielcosta/gocine/ui/activity/MainActivity.java
+++ app/src/main/java/io/gabrielcosta/gocine/ui/activity/MainActivity.java
@@ -1,7 +1,8 @@
-package io.gabrielcosta.gocine;
+package io.gabrielcosta.gocine.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import io.gabrielcosta.gocine.R;
 
 public class MainActivity extends AppCompatActivity {
 
