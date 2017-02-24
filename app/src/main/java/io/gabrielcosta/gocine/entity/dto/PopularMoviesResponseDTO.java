261a3e81f2072e424135b87ad65a819c7074e5eb
Gabriel Costa <gabriel.costa@concrete.com.br>
2017-02-24 15:00:24 -0300

    Change attribut type to list of PopularMovie

--- app/src/main/java/io/gabrielcosta/gocine/entity/dto/PopularMoviesResponseDTO.java
+++ app/src/main/java/io/gabrielcosta/gocine/entity/dto/PopularMoviesResponseDTO.java
@@ -2,6 +2,7 @@ package io.gabrielcosta.gocine.entity.dto;
 
 import com.google.gson.annotations.SerializedName;
 import io.gabrielcosta.gocine.entity.vo.PopularMovieResponseVO;
+import java.util.List;
 
 /**
  * Created by gabrielcosta on 23/02/17.
@@ -12,14 +13,14 @@ public final class PopularMoviesResponseDTO {
   @SerializedName("page")
   private final int page;
   @SerializedName("results")
-  private final PopularMovieResponseVO popularMovie;
+  private final List<PopularMovieResponseVO> popularMovie;
   @SerializedName("total_results")
   private final int totalResults;
   @SerializedName("total_pages")
   private final int totalPages;
 
   private PopularMoviesResponseDTO(int page,
-      PopularMovieResponseVO popularMovie, int totalResults, int totalPages) {
+      List<PopularMovieResponseVO> popularMovie, int totalResults, int totalPages) {
     this.page = page;
     this.popularMovie = popularMovie;
     this.totalResults = totalResults;
