72c09d5c47fabcdd1f9f776f2c7ad197c0266cf0
Gabriel Costa <gabriel.costa@concrete.com.br>
2017-02-24 16:19:03 -0300

    Create presenter for MainActivity

---
+++ app/src/main/java/io/gabrielcosta/gocine/presenter/MainPresenter.java
@@ -0,0 +1,46 @@
+package io.gabrielcosta.gocine.presenter;
+
+import io.gabrielcosta.gocine.entity.dto.PopularMoviesResponseDTO;
+import io.gabrielcosta.gocine.model.service.PopularMovieServiceImpl;
+import io.gabrielcosta.gocine.view.MainView;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+/**
+ * Created by gabriel on 2/24/17.
+ */
+
+public final class MainPresenter {
+
+  private final MainView view;
+  private final PopularMovieServiceImpl popularMovieService;
+
+
+  private MainPresenter(MainView view) {
+    this.view = view;
+    popularMovieService = new PopularMovieServiceImpl();
+  }
+
+  public static MainPresenter newInstance(final MainView view) {
+    return new MainPresenter(view);
+  }
+
+  public void fetchPopularMovies() {
+    popularMovieService.fetchPopularMovies().enqueue(new Callback<PopularMoviesResponseDTO>() {
+      @Override
+      public void onResponse(Call<PopularMoviesResponseDTO> call,
+          Response<PopularMoviesResponseDTO> response) {
+        if (response.code() == 200) {
+          view.setPopularMovieList(response.body().getPopularMovie());
+        }
+      }
+
+      @Override
+      public void onFailure(Call<PopularMoviesResponseDTO> call, Throwable t) {
+        view.onError(t.getMessage());
+      }
+    });
+  }
+
+}
