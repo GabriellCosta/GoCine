f18c323cc0cf8f783b5c76eb5a1b7871c89f9b91
Gabriel Costa <gabrielcosta08@gmail.com>
2017-02-26 21:19:32 -0300

    Make activity implement BaseActivity

--- app/src/main/java/io/gabrielcosta/gocine/ui/activity/MainActivity.java
+++ app/src/main/java/io/gabrielcosta/gocine/ui/activity/MainActivity.java
@@ -1,7 +1,6 @@
 package io.gabrielcosta.gocine.ui.activity;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import io.gabrielcosta.gocine.R;
@@ -12,7 +11,7 @@ import io.gabrielcosta.gocine.util.EndlessRecyclerOnScrollListener;
 import io.gabrielcosta.gocine.view.MainView;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements MainView {
+public class MainActivity extends BaseActivity implements MainView {
 
   private MainPresenter mainPresenter;
   private final int GRID_SIZE = 2;
