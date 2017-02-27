f3334c89c60e280f2917d09567da7b2139889942
Gabriel Costa <gabrielcosta08@gmail.com>
2017-02-26 21:19:27 -0300

    Create BaseActivity

---
+++ app/src/main/java/io/gabrielcosta/gocine/ui/activity/BaseActivity.java
@@ -0,0 +1,11 @@
+package io.gabrielcosta.gocine.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by gabrielcosta on 26/02/17.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+}
