4fd65cdcd96ccb0f8d69011ac6d4b0a44625dcc9
Gabriel Costa <gabrielcosta08@gmail.com>
2017-03-01 00:34:44 -0300

    Create method for parsing API Error's

--- app/src/main/java/io/gabrielcosta/gocine/model/retrofit/AbstractRetrofitServiceCall.java
+++ app/src/main/java/io/gabrielcosta/gocine/model/retrofit/AbstractRetrofitServiceCall.java
@@ -1,5 +1,10 @@
 package io.gabrielcosta.gocine.model.retrofit;
 
+import io.gabrielcosta.gocine.entity.vo.ErrorApiVO;
+import java.io.IOException;
+import java.lang.annotation.Annotation;
+import okhttp3.ResponseBody;
+import retrofit2.Converter;
 import retrofit2.Retrofit;
 
 /**
@@ -16,6 +21,17 @@ public abstract class AbstractRetrofitServiceCall {
 
   protected <T> T getService(Class<T> t) {
     return retrofitModel.create(t);
+  }
+
+  public ErrorApiVO parseError(final ResponseBody responseBody) {
+    Converter<ResponseBody, ErrorApiVO> converter = retrofitModel
+        .responseBodyConverter(ErrorApiVO.class, new Annotation[0]);
+
+    try {
+      return converter.convert(responseBody);
+    } catch (IOException e) {
+      return new ErrorApiVO(null, 0);
+    }
   }
 
 }
