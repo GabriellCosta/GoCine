f95e70958f56572ff3ee00322596f7fd2cf95c9e
Gabriel Costa <gabriel.costa@concrete.com.br>
2017-03-01 15:15:50 -0300

    Create Reviews service

--- app/src/main/java/io/gabrielcosta/gocine/model/service/MoviesService.java
+++ app/src/main/java/io/gabrielcosta/gocine/model/service/MoviesService.java
@@ -2,8 +2,10 @@ package io.gabrielcosta.gocine.model.service;
 
 import io.gabrielcosta.gocine.entity.dto.PaginatedResponseDTO;
 import io.gabrielcosta.gocine.entity.vo.MoviesResponseVO;
+import io.gabrielcosta.gocine.entity.vo.ReviewVO;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 import retrofit2.http.Url;
 
@@ -15,8 +17,12 @@ interface MoviesService {
 
   String MOVIE_POPULAR_ENDPOINT = "movie/popular";
   String MOVIE_TOP_RATED_ENDPOINT = "movie/top_rated";
+  String REVIEW_ENDPOINT = "movie/{id}/reviews";
 
   @GET
   Call<PaginatedResponseDTO<MoviesResponseVO>> fetchMovies(@Url String url, @Query("page") int page);
+
+  @GET(REVIEW_ENDPOINT)
+  Call<PaginatedResponseDTO<ReviewVO>> fetchReviews(@Path("id") final int id);
 
 }
--- app/src/main/java/io/gabrielcosta/gocine/model/service/MoviesServiceImpl.java
+++ app/src/main/java/io/gabrielcosta/gocine/model/service/MoviesServiceImpl.java
@@ -2,6 +2,7 @@ package io.gabrielcosta.gocine.model.service;
 
 import io.gabrielcosta.gocine.entity.dto.PaginatedResponseDTO;
 import io.gabrielcosta.gocine.entity.vo.MoviesResponseVO;
+import io.gabrielcosta.gocine.entity.vo.ReviewVO;
 import io.gabrielcosta.gocine.model.retrofit.AbstractRetrofitServiceCall;
 import io.gabrielcosta.gocine.model.retrofit.RetrofitModelImpl;
 import retrofit2.Call;
@@ -25,6 +26,10 @@ public class MoviesServiceImpl extends AbstractRetrofitServiceCall {
 
   public Call<PaginatedResponseDTO<MoviesResponseVO>> fetchMovies(final int page, final MovieEndpointType listType) {
     return retrofit.fetchMovies(listType.getEndpoint(), page);
+  }
+
+  public Call<PaginatedResponseDTO<ReviewVO>> fetchReviews(final int id) {
+    return retrofit.fetchReviews(id);
   }
 
 }
