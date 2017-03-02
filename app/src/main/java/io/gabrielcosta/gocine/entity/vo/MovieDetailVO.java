e6920f6a1688d768fd3d913b109f5914495892cf
Gabriel Costa <gabriel.costa@concrete.com.br>
2017-03-02 11:36:21 -0300

    Create a MovieDetailVO for detail endpoint service

---
+++ app/src/main/java/io/gabrielcosta/gocine/entity/vo/MovieDetailVO.java
@@ -0,0 +1,27 @@
+package io.gabrielcosta.gocine.entity.vo;
+
+import com.google.gson.annotations.SerializedName;
+import java.util.List;
+
+/**
+ * Created by gabriel on 3/2/17.
+ */
+
+public class MovieDetailVO extends MoviesResponseVO {
+
+  @SerializedName("runtime")
+  private final int runtime;
+
+  private MovieDetailVO(String posterPath, boolean adult, String overview,
+      String releaseDate, List<Integer> genreIds, int id, String originalTitle,
+      String originalLanguage, String title, String backdropPath, double popularity, int voteCount,
+      boolean video, double voteAverage, int runtime) {
+    super(posterPath, adult, overview, releaseDate, genreIds, id, originalTitle, originalLanguage,
+        title, backdropPath, popularity, voteCount, video, voteAverage);
+    this.runtime = runtime;
+  }
+
+  public int getRuntime() {
+    return runtime;
+  }
+}
