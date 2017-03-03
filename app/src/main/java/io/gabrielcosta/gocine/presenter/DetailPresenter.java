561cf22637933f8ef69bd2477ff56cf1ddfc8787
Gabriel Costa <gabrielcosta08@gmail.com>
2017-03-03 01:04:20 -0300

    Implement detail feature for presenter

--- app/src/main/java/io/gabrielcosta/gocine/presenter/DetailPresenter.java
+++ app/src/main/java/io/gabrielcosta/gocine/presenter/DetailPresenter.java
@@ -4,11 +4,16 @@ import io.gabrielcosta.gocine.BuildConfig;
 import io.gabrielcosta.gocine.entity.dto.PaginatedResponseDTO;
 import io.gabrielcosta.gocine.entity.dto.VideoResponseDTO;
 import io.gabrielcosta.gocine.entity.vo.ErrorApiVO;
+import io.gabrielcosta.gocine.entity.vo.MovieDetailVO;
 import io.gabrielcosta.gocine.entity.vo.ReviewVO;
 import io.gabrielcosta.gocine.entity.vo.VideoVO;
 import io.gabrielcosta.gocine.model.service.MoviesServiceImpl;
 import io.gabrielcosta.gocine.view.DetailView;
 import java.net.HttpURLConnection;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -79,6 +84,45 @@ public final class DetailPresenter {
     });
   }
 
+  public void getMovieDetail(final int movieId) {
+    movieService.getMovieDetail(movieId).enqueue(new Callback<MovieDetailVO>() {
+      @Override
+      public void onResponse(Call<MovieDetailVO> call, Response<MovieDetailVO> response) {
+        if (response.code() == HttpURLConnection.HTTP_OK) {
+          sucessDetail(response.body());
+        } else {
+          view.onError(movieService.parseError(response.errorBody()));
+        }
+      }
+
+      @Override
+      public void onFailure(Call<MovieDetailVO> call, Throwable t) {
+        view.onError(genericError());
+      }
+    });
+  }
+
+  private void sucessDetail(final MovieDetailVO detail) {
+    view.setMovieTitle(detail.getTitle());
+    view.setMovieDescription(detail.getOverview());
+    view.setMovieBackgroundImage(detail.getBackdropPath());
+    view.setMoviePoster(detail.getPosterPath());
+    view.setMovieDuration(detail.getRuntime());
+    view.setMovieRatio(detail.getVoteAverage());
+    setMovieYear(detail);
+  }
+
+  private void setMovieYear(MovieDetailVO detail) {
+    try {
+      final Date parse = SimpleDateFormat.getInstance().parse(detail.getReleaseDate());
+      final Calendar calendar = Calendar.getInstance();
+      calendar.setTime(parse);
+      view.setMovieYear(calendar.get(Calendar.YEAR));
+    } catch (ParseException e) {
+      e.printStackTrace();
+    }
+  }
+
   private void sucessVideo(final VideoResponseDTO response) {
     List<VideoVO> result = response.getResult();
     if (!result.isEmpty()) {
@@ -96,7 +140,7 @@ public final class DetailPresenter {
       view.setEmptyReviews();
     }
   }
-  
+
   private ErrorApiVO genericError() {
     return new ErrorApiVO(SERVICO_INDISPONIVEL, 0);
   }
