91df181d999095c1ff2ab98148d32de24c906578
Gabriel Costa <gabrielcosta08@gmail.com>
2017-03-05 13:18:38 -0300

    Create adapter for videos

---
+++ app/src/main/java/io/gabrielcosta/gocine/adapter/VideoAdapter.java
@@ -0,0 +1,54 @@
+package io.gabrielcosta.gocine.adapter;
+
+import android.support.v7.widget.RecyclerView.Adapter;
+import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import io.gabrielcosta.gocine.R;
+import io.gabrielcosta.gocine.adapter.VideoAdapter.VideoVH;
+import io.gabrielcosta.gocine.entity.vo.VideoVO;
+import java.util.List;
+
+/**
+ * Created by gabrielcosta on 05/03/17.
+ */
+
+public final class VideoAdapter extends Adapter<VideoVH> {
+
+  private final List<VideoVO> videoVOList;
+
+  public VideoAdapter(final List<VideoVO> videoVOList) {
+    this.videoVOList = videoVOList;
+  }
+
+  @Override
+  public VideoVH onCreateViewHolder(ViewGroup parent, int viewType) {
+    View inflate = LayoutInflater.from(parent.getContext())
+        .inflate(R.layout.list_item_video, parent, Boolean.FALSE);
+    return new VideoVH(inflate);
+  }
+
+  @Override
+  public void onBindViewHolder(VideoVH holder, int position) {
+    final VideoVO videoVO = videoVOList.get(position);
+    holder.name.setText(videoVO.getName());
+  }
+
+  @Override
+  public int getItemCount() {
+    return videoVOList.size();
+  }
+
+  final class VideoVH extends ViewHolder {
+
+    private final TextView name;
+
+    private VideoVH(View itemView) {
+      super(itemView);
+      name = (TextView) itemView.findViewById(R.id.textview_list_item_video_name);
+    }
+  }
+
+}
