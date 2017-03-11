09e254a062eb3df93f7dba777e3d8070f088ec8d
Gabriel Costa <gabrielcosta08@gmail.com>
2017-03-11 20:16:14 -0300

    Create connectionError method

--- app/src/main/java/io/gabrielcosta/gocine/ui/activity/BaseActivity.java
+++ app/src/main/java/io/gabrielcosta/gocine/ui/activity/BaseActivity.java
@@ -4,6 +4,8 @@ import android.support.annotation.StringRes;
 import android.support.design.widget.BaseTransientBottomBar;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View.OnClickListener;
+import io.gabrielcosta.gocine.R;
 
 /**
  * Created by gabrielcosta on 26/02/17.
@@ -16,12 +18,21 @@ public class BaseActivity extends AppCompatActivity {
   }
 
   void showError(final String message) {
-    Snackbar.make(findViewById(android.R.id.content), message, BaseTransientBottomBar.LENGTH_INDEFINITE)
+    Snackbar
+        .make(findViewById(android.R.id.content), message, BaseTransientBottomBar.LENGTH_INDEFINITE)
         .show();
   }
 
   void showError(@StringRes final int message) {
-    Snackbar.make(findViewById(android.R.id.content), message, BaseTransientBottomBar.LENGTH_INDEFINITE)
+    Snackbar
+        .make(findViewById(android.R.id.content), message, BaseTransientBottomBar.LENGTH_INDEFINITE)
+        .show();
+  }
+
+  void showConnectionError(final OnClickListener onClickListener) {
+    Snackbar.make(findViewById(android.R.id.content), R.string.no_internet_connection,
+        BaseTransientBottomBar.LENGTH_INDEFINITE)
+        .setAction(R.string.all_try_again, onClickListener)
         .show();
   }
 
